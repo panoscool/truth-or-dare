dba114abde20f0763f41e8c054f31566e8ffe8e0
Panos K <lkullolli@gmail.com>
2018-07-12 14:34:15 +0400

    Questions and Styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { truth, dare } from './components/questions';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="player">
-            player name
-          </div>
-          <div className="next-player">
-            next player
+            <div className="timer">
+              <h3>timer</h3>
+            </div>
+            <div className="current-player">
+              <h3>current player</h3>
+            </div>
+            <div className="next-player">
+              <h3>next player</h3>
           </div>
           <div className="questions">
-            questions
+            {truth.map((quest, index) => <h3 key={index}>{quest.question}</h3>)}
+            {dare.map((quest, index) => <h3 key={index}>{quest.question}</h3>)}
           </div>
           <button className="btn">&rarr;</button>
       </div>
---
+++ src/components/questions.js
@@ -0,0 +1,15 @@
+const truth = [
+    {question: "1"},
+    {question: "2"},
+    {question: "3"},
+    {question: "4"}
+]
+
+const dare = [
+    {question: "5"},
+    {question: "6"},
+    {question: "7"},
+    {question: "8"}
+]
+
+export {truth, dare};
