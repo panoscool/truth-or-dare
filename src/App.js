d2a79c2fd09f9b677de89b5cd21604269aa9e776
Panos K <lkullolli@gmail.com>
2018-07-12 15:48:56 +0400

    Countdown timer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import { truth, dare } from './components/questions';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: 0,
+    }
+  }
+
+  increment = () => {
+    this.setState({timer: this.state.timer +1})
+  }
+
+  decrement = () => {
+    this.setState({timer: this.state.timer -1})
+  }
+
   render() {
     return (
       <div className="App">
             <div className="timer">
-              <h3>timer</h3>
+              <button className="btn-timer" onClick={this.decrement}>-</button>
+                {this.state.timer}
+              <button className="btn-timer" onClick={this.increment}>+</button> 
+              <br />
+              <button className="btn-start">Start</button>
             </div>
             <div className="current-player">
               <h3>current player</h3>
@@ -19,7 +38,8 @@ class App extends Component {
             {truth.map((quest, index) => <h3 key={index}>{quest.question}</h3>)}
             {dare.map((quest, index) => <h3 key={index}>{quest.question}</h3>)}
           </div>
-          <button className="btn">&rarr;</button>
+          <button className="btn-truth">Truth</button>
+          <button className="btn-dare">Dare</button>
       </div>
     );
   }
