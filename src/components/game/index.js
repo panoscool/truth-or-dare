5104c00d6ecea576b68f7703f44c9fd615145b5b
Panos K <lkullolli@gmail.com>
2018-09-08 23:20:12 +0400

    local storage

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,17 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { truth, dare } from '../questions';
-import CountdownTimer from '../timer';
-import ShowQuest from './show-quest';
-
-const players = [
-  "Anna",
-  "John",
-  "Jackie",
-  "Maria",
-  "Sam",
-  "Eliz"
-]
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { truth, dare } from "../questions";
+import CountdownTimer from "../timer";
+import ShowQuest from "./show-quest";
 
 class Game extends Component {
   constructor() {
@@ -19,46 +10,48 @@ class Game extends Component {
     this.state = {
       currPlayer: -1,
       currQuest: null,
-      currType: null,
-    }
+      currType: null
+    };
+
+    this.players = JSON.parse(localStorage.getItem("players"));
   }
 
   handleRandomTruth = () => {
     this.playerTurn();
     // let remTQuest = truth.filter(t => !t.hasGameeard)
-    let randomNum = Math.floor(Math.random() * truth.length)
+    let randomNum = Math.floor(Math.random() * truth.length);
     this.setState({
       currQuest: truth[randomNum],
       currType: "Truth"
-    })
-    truth.splice(randomNum, 1)
+    });
+    truth.splice(randomNum, 1);
     // truth[randomNum].hasAppeard = true
-  }
-  
+  };
+
   handleRandomDare = () => {
     this.playerTurn();
-   // let remDQuest = dare.filter(d => !d.hasAppeard)
-    let randomNum = Math.floor(Math.random() * dare.length)
+    // let remDQuest = dare.filter(d => !d.hasAppeard)
+    let randomNum = Math.floor(Math.random() * dare.length);
     this.setState({
       currQuest: dare[randomNum],
       currType: "Dare"
-    })
-    dare.splice(randomNum, 1)
+    });
+    dare.splice(randomNum, 1);
     // dare[randomNum].hasAppeard = true
-  }
+  };
 
   playerTurn = () => {
     let index = this.state.currPlayer;
-    index = index+1;
+    index = index + 1;
 
-    if(players[index]) {
-      this.setState({ currPlayer: index});
+    if (this.players[index]) {
+      this.setState({ currPlayer: index });
     } else {
       // next question
-      this.setState({ currPlayer: 0 })
+      this.setState({ currPlayer: 0 });
       // TODO: go to next question
     }
-  }
+  };
 
   render() {
     //  {truth.map((quest, index) => <h3 key={index}>{quest.question}</h3>)}
@@ -66,23 +59,34 @@ class Game extends Component {
     return (
       <div className="tod">
         <div className="logo">
-          <img alt="logo" src={`${process.env.PUBLIC_URL}/images/logo.png`} height="70" width="120" />
+          <img
+            alt="logo"
+            src={`${process.env.PUBLIC_URL}/images/logo.png`}
+            height="70"
+            width="120"
+          />
         </div>
         <div className="timer">
           <CountdownTimer />
         </div>
 
         <div className="questions">
-           <ShowQuest 
-              currQuest={this.state.currQuest} 
-              currType={this.state.currType} 
-              currPlayer={players[this.state.currPlayer]}
-            />
+          <ShowQuest
+            currQuest={this.state.currQuest}
+            currType={this.state.currType}
+            currPlayer={this.players[this.state.currPlayer]}
+          />
         </div>
 
-        <button className="btn btn-truth" onClick={this.handleRandomTruth}>Truth ({truth.length})</button>
-        <button className="btn btn-dare" onClick={this.handleRandomDare}>Dare ({dare.length})</button>
-        <Link className="btn btn-home" to="/">Home</Link>
+        <button className="btn btn-truth" onClick={this.handleRandomTruth}>
+          Truth ({truth.length})
+        </button>
+        <button className="btn btn-dare" onClick={this.handleRandomDare}>
+          Dare ({dare.length})
+        </button>
+        <Link className="btn btn-home" to="/">
+          Home
+        </Link>
       </div>
     );
   }
--- src/components/players/index.js
+++ src/components/players/index.js
@@ -1,57 +1,73 @@
-import React, { Component } from 'react';
-import PlayersList from './players-list';
-import InputForm from './input-form';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import PlayersList from "./players-list";
+import InputForm from "./input-form";
+import { Link } from "react-router-dom";
 
 class Players extends Component {
-    constructor() {
-        super();
-        this.state = {
-          name: '',
-          players: []
-        }
-      }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      players: JSON.parse(localStorage.getItem("players")) || []
+    };
+  }
 
-    handleChange = (e) => {
-        this.setState({ name: e.target.value })
-    }
-    
-    handleSubmit = (e) => {
-        e.preventDefault();
-        if(!this.state.name.trim()) { return }
-        let newPlayer = this.state.players
-        newPlayer.push(this.state.name)
-        this.setState({
-            name: '',
-            players: newPlayer
-        })
-    }
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
 
-    deletePlayer = (index) => {
-        let newPlayer = this.state.players
-        newPlayer.splice(index, 1)
-        this.setState({ players: newPlayer })
+  handleSubmit = e => {
+    e.preventDefault();
+    if (!this.state.name.trim()) {
+      return;
     }
+    let newPlayer = this.state.players;
+    newPlayer.push(this.state.name);
+    this.setState(
+      {
+        name: "",
+        players: newPlayer
+      },
+      this.refreshStorage
+    );
+  };
 
-    render() {
-        return(
-          <div className="players">
-            <InputForm
-               handleChange={this.handleChange}
-               handleSubmit={this.handleSubmit}
-               value={this.state.value}
-               length={this.state.players.length +1}
+  deletePlayer = index => {
+    let newPlayer = this.state.players;
+    newPlayer.splice(index, 1);
+    this.setState({ players: newPlayer }, this.refreshStorage);
+  };
+
+  refreshStorage = () => {
+    localStorage.setItem("players", JSON.stringify(this.state.players));
+  };
+
+  render() {
+    return (
+      <div className="players">
+        <InputForm
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          value={this.state.name}
+          length={this.state.players.length + 1}
+        />
+
+        {this.state.players.map((playerName, index) => {
+          return (
+            <PlayersList
+              key={index}
+              playerName={playerName}
+              deletePlayer={this.deletePlayer}
             />
-            {this.state.players.map((playerName, index) => {
-               return <PlayersList 
-               key={index} 
-               playerName={playerName} 
-               deletePlayer={this.deletePlayer}
-            /> })}
-            <Link className="btn btn-play" to="/truth-or-dare">Play</Link>
-           </div>
-        );
-    }
+          );
+        })}
+
+        <Link className="btn btn-play" to="/truth-or-dare">
+          Play
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Players;
