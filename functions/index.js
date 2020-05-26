b6d0710282e01ea15d13d3ad48bf90c3450b2746
Panos K <lkullolli@gmail.com>
2020-05-26 15:08:59 +0400

    updated packages and node ver for firebase

--- functions/index.js
+++ functions/index.js
@@ -9,13 +9,13 @@ exports.addAdminRole = functions.https.onCall((data, context) => {
   }
 
   // get user and add custom claim (admin)
-  return admin.auth().getUserByEmail(data.email).then(user => {
+  return admin.auth().getUserByEmail(data.email).then((user) => {
     return admin.auth().setCustomUserClaims(user.uid, {
       admin: true
     });
   }).then(() => {
     return { message: `Success! ${data.email} has been made an admin.` };
-  }).catch(err => {
+  }).catch((err) => {
     return err;
   });
 });
