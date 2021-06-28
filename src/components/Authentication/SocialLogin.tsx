014a8bbc7f9af14377238a67c1d25a70655e149f
Panos K <lkullolli@gmail.com>
2021-06-28 15:00:11 +0400

    Added twitter login

--- src/components/Authentication/SocialLogin.tsx
+++ src/components/Authentication/SocialLogin.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button, Typography } from '@material-ui/core';
-import Facebook from '../Icons/Facebook';
-import Google from '../Icons/Google';
-import useAuthentication from '../../hooks/useAuthentication';
-import useTheme from '../../hooks/useTheme';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Typography } from "@material-ui/core";
+import Google from "../Icons/Google";
+import Facebook from "../Icons/Facebook";
+import Twitter from "../Icons/Twitter";
+import useAuthentication from "../../hooks/useAuthentication";
+import useTheme from "../../hooks/useTheme";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    textAlign: 'center',
-    margin: theme.spacing(2, 0)
+    textAlign: "center",
+    margin: theme.spacing(2, 0),
   },
   button: {
-    margin: theme.spacing(1, 0)
-  }
+    margin: theme.spacing(1, 0),
+  },
 }));
 
 function SocialLogin() {
@@ -26,7 +27,6 @@ function SocialLogin() {
       setModal(null);
 
       await socialLogin(selectedProvider);
-
     } catch (err) {
       console.error(err);
     }
@@ -35,8 +35,36 @@ function SocialLogin() {
   return (
     <div className={classes.root}>
       <Typography>- OR -</Typography>
-      <Button onClick={() => handleSocialLogin('facebook')} startIcon={<Facebook />} className={classes.button} fullWidth color="primary" variant="outlined">Continue with Facebook</Button>
-      <Button onClick={() => handleSocialLogin('google')} startIcon={<Google />} className={classes.button} fullWidth color="default" variant="outlined">Continue with Google</Button>
+      <Button
+        onClick={() => handleSocialLogin("google")}
+        startIcon={<Google />}
+        className={classes.button}
+        fullWidth
+        color="default"
+        variant="outlined"
+      >
+        Continue with Google
+      </Button>
+      <Button
+        onClick={() => handleSocialLogin("facebook")}
+        startIcon={<Facebook />}
+        className={classes.button}
+        fullWidth
+        color="primary"
+        variant="outlined"
+      >
+        Continue with Facebook
+      </Button>
+      <Button
+        onClick={() => handleSocialLogin("twitter")}
+        startIcon={<Twitter />}
+        className={classes.button}
+        fullWidth
+        color="primary"
+        variant="outlined"
+      >
+        Continue with Facebook
+      </Button>
     </div>
   );
 }
--- src/components/Icons/Facebook.jsx
+++ src/components/Icons/Facebook.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 // Material components
-import { SvgIcon } from '@material-ui/core';
+import { SvgIcon } from "@material-ui/core";
 
 export default function Facebook(props) {
   return (
